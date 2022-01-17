e7b01f8f0a5411097f677c9d23df9deec153f5cf
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-01-17 14:03:14 +0100

    ListTest

---
+++ List.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+var num = new TurboList<int> {8, 7, 0, 6, 1, 0, 8, 3, 3, 2};
+var enumerator = num.GetEnumerator();
+class TurboList<T> : IEnumerable {
+	private T[] elements;
+	private int count;
+
+	private int Count => elements.Length;
+
+	private void Add(T item) {
+		Array.Resize(ref elements, elements.Length + 1);
+		elements[elements.Length] = item;
+	}
+
+	private T Get(int index) {
+		return elements[index];
+	}
+
+	private void Clear() {
+		Array.Clear();
+	}
+
+	private void RemoveAt(int index) {
+		
+	}
+
+	bool Containts(T item) {
+		return false;
+	}
+
+	int IndexOf(T item) {
+		return 0;
+	}
+
+	void Remove(T item) {
+		
+	}
+
+	void AddRange(IEnumerable<T> items) {
+		
+	}
+
+	public IEnumerator GetEnumerator() {
+		throw new NotImplementedException();
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
-﻿using System;using System.Collections;
-using System.Collections.Generic;
-
-List<int> list = new List<int> {8, 7, 0, 6, 1, 0, 8, 3, 3, 2};
-	
-IEnumerable collection = list;
-IEnumerator enumerator = collection.GetEnumerator();
-
-while (enumerator.MoveNext()) {
-	Console.WriteLine(enumerator.Current);
-}
+﻿// using System;
+// using System.Collections.Generic;
+//
+// var perNum = new List<int> {8, 7, 0, 6, 1, 0, 8, 3, 3, 2};
+// using var enumerator = perNum.GetEnumerator();
+// var sum = 0;
+//
+// while (enumerator.MoveNext()) {
+// 	Console.WriteLine(enumerator.Current);
+// 	sum += enumerator.Current;
+// }
+// Console.WriteLine(sum);
+//
+// foreach (var number in perNum) {
+// 	Console.WriteLine(number);
+// }
 
