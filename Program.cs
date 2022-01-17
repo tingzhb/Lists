f03ae11b5d2f073435bb81885daf2396f990163d
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-01-17 10:57:00 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;using System.Collections;
+using System.Collections.Generic;
+
+List<int> list = new List<int> {8, 7, 0, 6, 1, 0, 8, 3, 3, 2};
+	
+IEnumerable collection = list;
+IEnumerator enumerator = collection.GetEnumerator();
+
+while (enumerator.MoveNext()) {
+	Console.WriteLine(enumerator.Current);
+}
+
